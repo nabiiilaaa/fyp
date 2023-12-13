0dc88d690d9fec4135e2f1c28ae28ef13bfebe9e
nabiiilaaahampton.ac.uk <shimlan@roehampton.ac.uk>
2023-12-13 23:19:44 +0000

    more improvement done

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -7,15 +7,43 @@ function loadSection(section) {
         .then(response => response.text())
         .then(data => {
             document.getElementById('content-section').innerHTML = data;
+            if (section !== 'about-us') {
+                removeImageAndHeader();
+            }
         })
         .catch(error => console.error('Error loading section:', error));
+}
 
-        document.getElementById('addBookBtn').addEventListener('click', function() {
-            ////////
-        });
-        
-        document.getElementById('addListingBtn').addEventListener('click', function() {
-            /////////
-        });
-        
+function removeImageAndHeader() {
+    // Remove the image
+    var logoElement = document.querySelector('.logo');
+    if (logoElement) {
+        logoElement.remove();
+    }
+
+    // Remove the header
+    var headerElement = document.querySelector('header');
+    if (headerElement) {
+        headerElement.remove();
+    }
+}
+
+function showForm(formType) {
+    if (formType === 'signup') {
+        document.getElementById('signup-form').style.display = 'block';
+        document.getElementById('login-form').style.display = 'none';
+    } else if (formType === 'login') {
+        document.getElementById('signup-form').style.display = 'none';
+        document.getElementById('login-form').style.display = 'block';
+    }
+}
+
+function validateSignupForm() {
+    // Add validation logic for signup form
+    return true; // Return true if the form is valid
+}
+
+function validateLoginForm() {
+    // Add validation logic for login form
+    return true; // Return true if the form is valid
 }
