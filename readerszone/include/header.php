95bd80f243bde6f0babea3e56be608dde883c441
nabiiilaaahampton.ac.uk <shimlan@roehampton.ac.uk>
2024-01-09 15:29:46 +0000

    logo updated

--- readerszone/include/header.php
+++ readerszone/include/header.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
         <div class="container-fluid">
             <div class="w3-bar w3-border w3-card-4 w3-light-grey">
-                <a class="w3-bar-item w3-button" href="home.php" style="padding: 0px; margin: 0px; margin-right: 10px;"><img src="/ReadersZone/wwwroot/images/logo1a.png" height="100%"/><img src="/ReadersZone/wwwroot/images/logo1b.png" height="100%"/></a>
+                <a class="w3-bar-item w3-button" href="home.php" style="padding: 0px; margin: 0px; margin-right: 10px;"><img src="/ReadersZone/wwwroot/images/logo.png" height="100%"/><img src="/ReadersZone/wwwroot/images/logo1b.png" height="100%"/></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="float: right;">
                     <span class="navbar-toggler-icon"></span>
                 </button>
