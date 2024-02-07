f156b33320e389a89c7a8523987445ea9b88d554
nabiiilaaahampton.ac.uk <shimlan@roehampton.ac.uk>
2024-02-07 12:42:21 +0000

    managing user ratings and reviews for books

--- readerszone/dal/dbController.php
+++ readerszone/dal/dbController.php
@@ -34,5 +34,57 @@ class DBController {
 		$rowcount = mysqli_num_rows($result);
 		return $rowcount;	
 	}
+
+	function addUserRating($userId, $bookId, $rating)
+	{
+		if($this->userRating($userId, $bookId, null) == null) {
+			$sql = "INSERT INTO reviews (userId, bookId, rating) VALUES ('$userId', '$bookId', '$rating')";
+			$this->runQueryNoReturn($sql);
+		} else {
+			$sql = "UPDATE reviews SET rating = '$rating' WHERE userId = '$userId' AND bookId = '$bookId'";
+			$this->runQueryNoReturn($sql);
+		}
+	}
+
+	function removeUserRating($userId, $bookId)
+	{
+		$sql = "DELETE FROM reviews WHERE userId = '$userId' AND bookId = '$bookId'";
+		$this->runQueryNoReturn($sql);
+	}
+
+	function userRating($userId, $bookId, $default)
+	{
+		$sql = "SELECT * FROM reviews WHERE userId = '$userId' AND bookId = '$bookId'";
+		$result = $this->runQuery($sql);
+		if (!empty($result)) {
+			foreach($result as $key=>$value) {
+				return $result[$key]["Rating"];
+			}
+		}
+		else {
+			return $default;
+		}
+	}
+
+	function avgRating($bookId, $default)
+	{
+		$sql = "SELECT * FROM reviews WHERE bookId = '$bookId'";
+		$result = $this->runQuery($sql);
+		$average = $default;
+		if (!empty($result)) {
+			$average = 0;
+			foreach($result as $key=>$value) {
+				$average += $result[$key]["Rating"];
+			}
+			$average /= $this->totalRating($bookId);
+		}
+		return $average;
+	}
+
+	function totalRating($bookId)
+	{
+		$sql = "SELECT rating FROM reviews WHERE bookId = '$bookId'";
+		return $this->numRows($sql);
+	}
 }
 ?>
